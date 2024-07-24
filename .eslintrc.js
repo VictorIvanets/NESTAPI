46b7f60f66e034ae0ac662f1fd922f9b4402ad52
unknown <imperia.zt@gmail.com>
2024-07-24 13:20:22 +0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+
+    "prettier",
+    "plugin:prettier/recommended"
+
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    
+
+    "no-empty-function":"off",
+    "@typescript-eslint/ban-types":"off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    "@typescript-eslint/no-explicit-any":"off",
+    "prettier/prettier": [
+        "error",
+        {
+            "singleQuote": true,
+            "trailingComma": "all",
+            "useTabs": true,
+            "semi": false,
+            "bracketSpacing": true,
+            "printWidth": 100,
+            "endOfLine": "auto"
+        }
+    ]
+
+
+  },
+};
